f01503f400519c09d34d6a46b83ee419ae36050d
Arseniy <b-ars07@yandex.ru>
2017-02-09 16:04:18 +0300

    Сверстал подменю и блок статьи

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(function() {
+
+  // //главный слайдер
   $('.bxslider').bxSlider({
     auto: true,
     speed: 1000,
     adaptiveHeightSpeed: 2000
   });
+  //каруселька
+  $('.slider1').bxSlider({
+    slideWidth: 350,
+    minSlides: 2,
+    maxSlides: 3,
+    moveSlides: 1,
+    slideMargin: 30
+  });
+
 });
