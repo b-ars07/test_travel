fd6591fa49edb16b09a5a959e0172754a4d32ff8
Arseniy <b-ars07@yandex.ru>
2017-02-09 21:08:36 +0300

    Сверстал главную страницу (поменял свайпер, правки по пиксель перфекту)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 $(function() {
 
-  // //главный слайдер
-  $('.bxslider').bxSlider({
-    auto: true,
-    speed: 1000,
-    adaptiveHeightSpeed: 2000
-  });
-  //каруселька
-  $('.slider1').bxSlider({
-    slideWidth: 350,
-    minSlides: 2,
-    maxSlides: 3,
-    moveSlides: 1,
-    slideMargin: 30
-  });
+  // // //главный слайдер
+  // $('.bxslider').bxSlider({
+  //   auto: true,
+  //   speed: 1000,
+  //   adaptiveHeightSpeed: 2000
+  // });
+  // //каруселька
+  // $('.slider1').bxSlider({
+  //   slideWidth: 350,
+  //   minSlides: 2,
+  //   maxSlides: 3,
+  //   moveSlides: 1,
+  //   slideMargin: 30
+  // });
+
+  var swiper = new Swiper('.first-slider', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: 7000,
+        effect: 'fade'
+    });
+
+    var carousel = new Swiper('.slider1', {
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true
+    });
 
 });
