6926ac1cd9603887aabb93def7ea323e6b7e00b5
Arseniy <b-ars07@yandex.ru>
2017-02-13 18:41:46 +0300

    Начал адаптив для планшетов, полностью подогнал верстку по писель перфект

--- js/main.js
+++ js/main.js
@@ -1,36 +1,51 @@
-$(function() {
+$(function () {
 
-  // // //главный слайдер
-  // $('.bxslider').bxSlider({
-  //   auto: true,
-  //   speed: 1000,
-  //   adaptiveHeightSpeed: 2000
-  // });
-  // //каруселька
-  // $('.slider1').bxSlider({
-  //   slideWidth: 350,
-  //   minSlides: 2,
-  //   maxSlides: 3,
-  //   moveSlides: 1,
-  //   slideMargin: 30
-  // });
 
-  var swiper = new Swiper('.first-slider', {
+    var swiper = new Swiper('.first-slider', {
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         slidesPerView: 1,
         spaceBetween: 30,
         loop: true,
+        autoHeight: true,
         autoplay: 7000,
         effect: 'fade'
+
     });
 
     var carousel = new Swiper('.slider1', {
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-      slidesPerView: 3,
-      spaceBetween: 30,
-      loop: true
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 3,
+        spaceBetween: 33,
+        loop: true,
+        breakpoints: {
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
     });
+
+    var $hamburger = $(".hamburger");
+    $hamburger.on("click", function (e) {
+        $hamburger.toggleClass("is-active");
+        // Do something else, like open/close menu
+    });
+
+
+
 
 });
